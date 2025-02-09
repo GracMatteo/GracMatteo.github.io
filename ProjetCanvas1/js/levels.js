6a1806369312dafc855500e8d1167f523c1bc096
GracMatteo <matteo.grac@etu.univ-cotedazur.fr>
2025-02-09 19:16:44 +0100

    3 niveaux acceptable

--- ProjetCanvas1/js/levels.js
+++ ProjetCanvas1/js/levels.js
@@ -1,33 +1,46 @@
 export const levels = [
     {
-        playerStart: { x: 0, y: 0 },
-        playerSouris : { x: 200, y: 200, w: 25, h: 25, couleur: "orange"},
-        sortie: { x: 700, y: 100, r: 15, couleur: "yellow" },
+        playerStart: { x: 100, y: 100 },
+        playerSouris: { x: 200, y: 200, w: 25, h: 25, couleur: "orange" },
+        sortie: { x: 600, y: 100, r: 15, couleur: "yellow" },
         obstacles: [
-            { x: 300, y: 0, w: 40, h: 600, couleur: "red" },
-            { x: 500, y: 500, w: 100, h: 100, couleur: "blue" }
+            { x: 300, y: 0, w: 50, h: 390, couleur: "red" },
+            { x: 480, y: 460, w: 120, h: 80, couleur: "blue" }
         ],
         obstaclesAnimes: [
-            { x: 200, y: 300, w: 50, h: 50, couleur: "green", vitesseX: 0, vitesseY: 2 },
-            { x: 500, y: 300, w: 50, h: 50, couleur: "green", vitesseX: 0, vitesseY: -2 }
+            { x: 100, y: 400, w: 50, h: 50, couleur: "green", vitesseX: 2, vitesseY: 0 }
+        ]
+    },
+    {
+        playerStart: { x: 50, y: 50 },
+        playerSouris: { x: 250, y: 250, w: 25, h: 25, couleur: "orange" },
+        sortie: { x: 700, y: 50, r: 15, couleur: "yellow" },
+        obstacles: [
+            { x: 250, y: 0, w: 50, h: 300, couleur: "red" },
+            { x: 480, y: 350, w: 100, h: 100, couleur: "blue" }
+        ],
+        obstaclesAnimes: [
+            { x: 100, y: 200, w: 50, h: 50, couleur: "green", vitesseX: 3, vitesseY: 0 },
+            { x: 500, y: 100, w: 50, h: 50, couleur: "green", vitesseX: 0, vitesseY: 3 }
         ]
     },
     
     {
-        playerStart: { x: 100, y: 100 },
-        playerSouris : { x: 200, y: 200, w: 25, h: 25, couleur: "orange"},
-        sortie: { x: 600, y: 100, r: 15 , couleur: "yellow" },
+        playerStart: { x: 50, y: 50 },
+        playerSouris: { x: 300, y: 400, w: 25, h: 25, couleur: "orange" },
+        sortie: { x: 750, y: 50, r: 15, couleur: "yellow" },
         obstacles: [
-            { x: 300, y: 0, w: 50, h: 390, couleur: "red" },
-            { x: 450, y: 460, w: 120, h: 80, couleur: "blue" }
+            { x: 200, y: 0, w: 60, h: 300, couleur: "red" },
+            { x: 400, y: 350, w: 120, h: 120, couleur: "blue" },
+            { x: 200, y: 550, w: 80, h: 100, couleur: "blue" },
+            { x: 500, y: 550, w: 80, h: 100, couleur: "blue" },
         ],
         obstaclesAnimes: [
-            { x: 100, y: 400, w: 50, h: 50, couleur: "green", vitesseX: 2, vitesseY: 0 },
-            { x: 400, y: 250, w: 50, h: 50, couleur: "green", vitesseX: -2, vitesseY: 0 },
-            { x: 700, y: 0, w: 50, h: 50, couleur: "green", vitesseX: 0, vitesseY: 2 }
+            { x: 200, y: 400, w: 50, h: 50, couleur: "green", vitesseX: 5, vitesseY: 0 },
+            { x: 450, y: 50, w: 50, h: 50, couleur: "green", vitesseX: 2, vitesseY: 2 },
+            { x: 500, y: 50, w: 50, h: 50, couleur: "green", vitesseX: 0, vitesseY: 2 }
         ]
-    },
-    
-    
-
+    }
 ];
+
+
